f06ee5ff0679a244126235b5e64cc00d13b90629
jamesleakos <jamesleakos@gmail.com>
2019-03-16 20:46:27 -0400

    Some aesthetic work including adding a glowing block - could be a disappearing block.

---
+++ Artemis/Assets/Scripts/ActorsNObjects/Items/Glowbox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glowbox : MonoBehaviour {
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        animator.speed = Random.Range(.8f, 1.2f);
+        //float size = Random.Range(.8f, 2.5f);
+        //Vector3 newSize = new Vector3(size, size, 1);
+        //transform.localScale = newSize;
+
+    }
+}
