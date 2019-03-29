df1bc3c491c536b57bca90ad23d5d43e1a97ad8f
jamesleakos <jamesleakos@gmail.com>
2019-03-29 00:12:56 -0400

    Adding singing to birds.

---
+++ Artemis/Assets/Scripts/ActorsNObjects/NPC/Flutterbird.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flutterbird : MonoBehaviour
+{
+
+    AudioSource chirping;
+    public float defaultVolume = 1;
+    public float defaultPitch = 1;
+    public float randomVolume = 0.3f;
+    public float randomPitch = 0.3f;
+
+    #region Player Target 
+    // get player target
+    Transform playerTransform;
+    Player player;
+    float nextTimeToSearch;
+    float searchInterval = 0.5f;
+    #endregion
+
+    void Start()
+    {
+        chirping = gameObject.GetComponent<AudioSource>();
+        chirping.volume = defaultVolume * PlayerPrefs.GetFloat("MainVolume") * PlayerPrefs.GetFloat("SFXVolume");
+    }
+
+    void Update() {
+        if (playerTransform == null) {
+            FindPlayer();
+        }
+        if (playerTransform != null) {
+            if ((playerTransform.position - transform.position).magnitude < chirping.maxDistance + 5f) {
+                if (!chirping.isPlaying) {
+                    chirping.volume = defaultVolume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f)) * PlayerPrefs.GetFloat("MainVolume") * PlayerPrefs.GetFloat("SFXVolume");
+                    chirping.pitch = defaultPitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+                    chirping.Play();
+                }
+            } else {
+                if (chirping.isPlaying) {
+                    chirping.Stop();
+                }
+            }
+        }
+    }
+
+    void FindPlayer() {
+        if (nextTimeToSearch <= Time.time) {
+            GameObject searchResult = GameObject.FindGameObjectWithTag("Player");
+            if (searchResult != null) {
+                player = searchResult.GetComponent<Player>();
+                playerTransform = searchResult.GetComponent<Transform>();
+            }
+            nextTimeToSearch = Time.time + searchInterval;
+        }
+    }
+}
